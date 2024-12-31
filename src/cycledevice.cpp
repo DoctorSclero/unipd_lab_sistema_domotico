7a71389b55145c8e607038addd299e1c47d87c0c
Diego Chiesurin <diego@Diegos-Macbook-Air.local>
2024-12-31 16:38:54 +0100

    diego - devices v1.0

---
+++ src/cycledevice.cpp
@@ -0,0 +1,17 @@
+#include "../include/cycledevice.h"
+
+/**
+* updates the state of the device:
+*  - turns on if time = start_time
+*  - turns off if time = start_time + cycle_duration
+*  - keeps current state otherwise
+* @param time, supposed to be logically correct
+* @return bool, true if device is on, false otherwise
+*/
+void CycleDevice::update(){
+    if(home.get_time() == start_time){
+        start();
+    }else if(home.get_time() == start_time + cycle_duration){
+        stop();
+    }
+}
