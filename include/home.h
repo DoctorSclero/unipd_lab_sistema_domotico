dcc1a7af674067f002ca44142899d67c9fb067b9
Pietro Ballarin <pieballa@gmail.com>
2024-12-31 16:37:35 +0100

    feat: created home

---
+++ include/home.h
@@ -0,0 +1,15 @@
+#ifndef DOMOTIC_DEVICES_HOME
+#define DOMOTIC_DEVICES_HOME
+
+namespace sistema_domotico {
+    
+    class Home {
+        private:
+            Logger logger;
+            int current_time;
+            
+    };
+
+}
+
+#endif
