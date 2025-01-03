6856f9a56acb5e6fbce9a766f793e2322ed68f48
Pietro Ballarin <pieballa@gmail.com>
2025-01-03 17:36:24 +0100

    feat: added logger class definition

--- include/logger.h
+++ include/logger.h
@@ -5,11 +5,28 @@
 #include "home.h";
 
 namespace domoticdevices {
+    /**
+     * Manages the logging of events of a house
+     */
     class Logger {
         private:
-            Home home;
-            std::ofstream file;
+            Home& p_home;
+            std::ofstream p_file;
         public:
+            /**
+             * Logger constructor
+             * @param file_path The log file path
+             * @param home A reference to the home for time retrival
+             */
+            Logger(std::string file_path, Home& home)
+            : p_home{home}, p_file{file_path} {};
+
+            /**
+             * Loggs an event by writing it in the console
+             * and on the specified log file.
+             * @param message The message to print
+             */
+            void log(std::string message);
     };
 }
 #endif
