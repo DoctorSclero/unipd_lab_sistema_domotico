e2b77cf84df0ec17cecd0280140d07d6a0a11354
Pietro Ballarin <pieballa@gmail.com>
2025-01-03 17:09:12 +0100

    feat: added namespaces, adjusted names to naming conventions

--- include/cycledevice.h
+++ include/cycledevice.h
@@ -1,16 +1,18 @@
-#ifndef CYCLEDEVICE_H
-#define CYCLEDEVICE_H
+#ifndef DOMOTIC_DEVICES_CYCLEDEVICE_H
+#define DOMOTIC_DEVICES_CYCLEDEVICE_H
 
 #include "device.h"
 
-class CycleDevice : public Device{
-    private:
-        int cycle_duration;
-    public:
-        CycleDevice(std::string n, double p, int pr, int cd)
-            : Device{n, p, pr}, cycle_duration{cd} {}
-            
-        void update();
-};
+namespace domoticdevices {
+    class CycleDevice : public Device{
+        private:
+            int cycle_duration;
+        public:
+            CycleDevice(std::string n, double p, int pr, int cd)
+                : Device{n, p, pr}, cycle_duration{cd} {}
+                
+            void update();
+    };
+}
 
 #endif
--- include/manualdevice.h
+++ include/manualdevice.h
@@ -1,16 +1,18 @@
-#ifndef MANUALDEVICE_H
-#define MANUALDEVICE_H
+#ifndef DOMOTIC_DEVICES_MANUALDEVICE_H
+#define DOMOTIC_DEVICES_MANUALDEVICE_H
 
 #include "device.h"
 
-class ManualDevice : public Device{
-    private:
-        int stop_time;
-    public:
-        ManualDevice(std::string n, double p, int pr)
-         : Device{n, p, pr}, stop_time{-1} {}
-         
-        bool update();
-};
+namespace domoticdevices {
+    class ManualDevice : public Device{
+        private:
+            int stop_time;
+        public:
+            ManualDevice(std::string n, double p, int pr)
+            : Device{n, p, pr}, stop_time{-1} {}
+            
+            bool update();
+    };
+}
 
 #endif
